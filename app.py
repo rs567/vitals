539d83996f5f3402dfa5af1521b613c8bd1518eb
Nathaniel Wong <57159408+rs567@users.noreply.github.com>
2025-04-12 19:31:32 -0700

    moved to flask

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/about")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/about")
+def about():
+    return "<p>This is the about page.</p>"
+
