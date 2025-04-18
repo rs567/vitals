3a70cd7276aead4f357ae38ecc5b00034ae36d17
ryowright <ryogwright@gmail.com>
2025-04-18 13:22:23 -0700

    Added Alerts and Appointments Accordions

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,16 +1,24 @@
+import VitalsNavbar from "./components/navbar";
+import { AlertsAccordion, AppointmentsAccordion } from "./components/accordion";
+
 export default function Home() {
   return (
-    <div className="bg-black h-screen p-10">
+    <div className="bg-black h-screen">
       <div className="h-full flex">
         <div className="flex-1/4 bg-white p-8 pr-4">
           <div className="h-full p-8 bg-gray-600 flex flex-col gap-4">
-            <div className="basis-2/3 bg-gray-400">1</div>
-            <div className="basis-2/3 bg-gray-400">2</div>
+            <div className="basis-2/3 bg-gray-400">
+              <AlertsAccordion />
+            </div>
+            <div className="basis-2/3 bg-gray-400">
+              <AppointmentsAccordion />
+            </div>
             <div className="basis-3/3 bg-gray-400">3</div>
           </div>
         </div>
         <div className="flex-3/4 bg-white p-8 pl-4">
           <div className="h-full p-8 bg-gray-600 flex flex-col gap-4">
+            <VitalsNavbar />
             <div className="basis-3/7 bg-gray-400">
               <div>5</div>
             </div>
