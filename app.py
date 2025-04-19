cded1e0389148c51ef66eff9344d1181fa87493a
Nathaniel Wong <57159408+rs567@users.noreply.github.com>
2025-04-19 15:39:50 -0700

    added upload, download endpoints

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from markupsafe import escape
+from db import MongoDBConnection
+from bson import ObjectId
 
 app = Flask(__name__)
-
-@app.route("/about")
-def hello_world():
-    return "<p>Hello, World!</p>"
+mongo = MongoDBConnection(db_name="testdb")
+mongo.connect()
 
 @app.route("/about")
 def about():
@@ -14,3 +14,33 @@ def about():
 @app.route("/<name>")
 def hello(name):
     return f"Hello, {escape(name)}!\n"
+
+@app.route("/upload_doc", methods=["POST"])
+def upload_document():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+
+    file_id = mongo.fs.put(file, filename=file.filename)
+    return jsonify({"message": "File uploaded", "file_id": str(file_id)})
+
+@app.route("/download/<file_id>", methods=["GET"])
+def download_document(file_id: ObjectId):
+    try:
+        grid_out = mongo.fs.get(ObjectId(file_id))
+        return (grid_out.read(), 
+                200, 
+                {'Content-Type': 'application/pdf',
+                 'Content-Disposition': f'attachment; filename={grid_out.filename}'})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 404
+    
+# @app.teardown_appcontext # This is running when I don't want it to
+# def close_connection(exception):
+#     mongo.close()
+
+if __name__ == "__main__":
+    app.run(debug=True)
